247d7a04425e25e6b0cc444ce4f173aa6ac294f0
Pc <Pc@DESKTOP-UDUDTTF>
2024-05-16 15:24:05 -0300

    actualización en form-cliente: menú de navegación

--- tp3_VediaLuciana/src/main/java/ar/edu/unju/fi/HomeController.java
+++ tp3_VediaLuciana/src/main/java/ar/edu/unju/fi/HomeController.java
@@ -4,7 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ClienteController {
+public class HomeController {
+
+    @GetMapping("/index")
+    public String home() {
+        return "index";
+    }
 
     @GetMapping("/form-cliente")
     public String formCliente() {
